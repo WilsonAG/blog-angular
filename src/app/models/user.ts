c3c1a0ef3d863b80fb097e8feee68241a33ab1fc
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-02-22 13:26:07 -0500

    Agregado librerias a usar y modelos.

---
+++ src/app/models/user.ts
@@ -0,0 +1,12 @@
+export class user {
+  constructor(
+    public id: number,
+    public name: string,
+    public lastname: string,
+    public role: string,
+    public email: string,
+    public password: string,
+    public description: string,
+    public image: string
+  ) {}
+}
